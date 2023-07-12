b4bf5b0f48b1d85e99b586b26dcc2c7949f60df4
root <root@MaxsPC>
2023-07-12 14:17:40 -0700

    Switching p tag while interviewing

--- src/components/common/ServicesCard.tsx
+++ src/components/common/ServicesCard.tsx
@@ -2,7 +2,8 @@ const ServicesCard = ({ title, image }: { title: string, content: string, image:
   return (
     <div className='flex flex-col w-full overflow-hidden bg-white shadow-md h-fit rounded-xl'>
       <img src={image} alt='' className='md:h-60 sm:36' />
-      <a href='#' className='p-2 text-xl text-center text-white transition-all duration-500 bg-gray-500 min-h-6 md:p-3 font-poppins hover:bg-blue-800'>{title}</a>
+      { /* Switch p tag back to a tag when it's clickable */ }
+      <p href='#' className='p-2 text-xl text-center text-white transition-all duration-500 bg-gray-500 min-h-6 md:p-3 font-poppins'>{title}</p>
     </div>
   )
 }
